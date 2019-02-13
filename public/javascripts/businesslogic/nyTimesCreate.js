5908d99a9c9ebfde2453a4873799a5e5ed0c86b8
qit108 <christofer.bergsten@teliacompany.com>
2019-02-13 21:39:35 +0100

    returning list of classes to FE

--- public/javascripts/businesslogic/nyTimesCreate.js
+++ public/javascripts/businesslogic/nyTimesCreate.js
@@ -1,12 +1,17 @@
-var express = require('express');
 var NyTimes = require('../model/NyTimes');
 var getNyTimesData = require('../../../routes/nytimes');
 
 let nyTimesCreate = async () => {
     let timesData = await getNyTimesData().then((res) => {return res});
-    const {snippet, source, pub_date, copyright, web_url} = timesData.response.docs[0];
 
-    return new NyTimes(snippet, source, copyright, pub_date, web_url);
+    let storeClass = [{}];
+    let test = timesData.response.docs;
+    test.forEach(i => {
+        let ni = new NyTimes(i.snippet, i.source, i.copyright, i.pub_date, i.web_url);
+        storeClass.push(ni);
+    });
+
+    return storeClass;
 }
 
 module.exports = nyTimesCreate;
